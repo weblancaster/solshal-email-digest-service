8b5332be88a6a5aaa468183501ccef547c5a6cfb
Michael Lancaster <michaell.llancaster@gmail.com>
2016-11-28 15:16:56 -0600

    Many improvements related to interval/cron job, email delivery and config.
    - Filter mongo query to only return needed fields
    - Implement logic to flatten user collections and format obj
    - Add sparkpost API_KEY to config file
    - Implement code interval to trigger email post
    - Implement listener to when process exit

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 'use strict';
 
 const MongoClient = require('mongodb').MongoClient;
+const moment = require('moment');
 const config = require('./lib/config/index');
 const digestCollection = require('./lib/jobs/digest');
+
+let digestTimer;
+const digestDelay = moment.duration(1, 'm').asMilliseconds(); // 1 minute delay
 
 // DB setup/start
 const uriDBString = config[process.env.NODE_ENV].db.uri;
 
 MongoClient.connect(uriDBString, config.dbOptions, (err, db) => {
-  if ( err ) {
+  if (err) {
     console.log(err);
     process.exit(1);
   }
-
-  digestCollection(db);
+  startInterval(db);
 });
+
+function startInterval(db) {
+  digestTimer = setInterval(() => {
+    console.log('called!');
+    digestCollection(db);
+  }, digestDelay);
+}
+
+// the code below will listen to
+// when the application exits
+// then clear the interval
+function exitHandler(options, err) {
+  if (options.cleanup) {
+    console.log('clean');
+  }
+  if (err) {
+    console.log(err.stack);
+  }
+  if (options.exit) {
+    process.exit();
+  }
+
+  clearInterval(digestTimer);
+}
+
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
